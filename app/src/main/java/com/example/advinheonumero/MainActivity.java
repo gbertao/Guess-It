003cd99c924f6b539d862b01236233d1e573f74d
Giovanni <gbertao96@gmail.com>
2021-06-28 23:03:38 -0300

    Fix #2

--- app/src/main/java/com/example/advinheonumero/MainActivity.java
+++ app/src/main/java/com/example/advinheonumero/MainActivity.java
@@ -25,13 +25,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        int random;
-        String strRandom;
+        int random, guessed = 0;
+        String strRandom, strGuessed;
 
         random = this.r.nextInt(1000);
         strRandom = String.format("%03d", random);
         TextView myAwesomeTextView = (TextView)findViewById(R.id.textView);
 
         myAwesomeTextView.setText(strRandom);
+
+        TextView tvNum = (TextView)findViewById(R.id.Input1);
+        strGuessed = tvNum.getText().toString();
+        if (strGuessed.equals(""))
+            strGuessed = "0";
+        guessed += 100*Integer.parseInt(strGuessed);
+
+        tvNum = (TextView)findViewById(R.id.Input2);
+        strGuessed = tvNum.getText().toString();
+        if (strGuessed.equals(""))
+            strGuessed = "0";
+        guessed += 10*Integer.parseInt(strGuessed);
+
+        tvNum = (TextView)findViewById(R.id.Input3);
+        strGuessed = tvNum.getText().toString();
+        if (strGuessed.equals(""))
+            strGuessed = "0";
+        guessed += Integer.parseInt(strGuessed);
+
+        strGuessed = String.format("%03d", guessed);
+
+        if (strGuessed.equals(strRandom))
+            myAwesomeTextView.setText("Acertou");
+        else
+            myAwesomeTextView.setText("Errou");
     }
 }
